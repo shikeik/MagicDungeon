6a2a692ffef66b8c1a932978bfd239852bee8678
shikeik <2667097598@qq.com>
2026-02-10 20:44:12 +0800

    fix: 删除bob游戏内容以及todo更新

--- examples/src/main/java/com/goldsprite/magicdungeon/screens/TestSelectionScreen.java
+++ examples/src/main/java/com/goldsprite/magicdungeon/screens/TestSelectionScreen.java
@@ -7,13 +7,11 @@ import com.goldsprite.magicdungeon.screens.tests.MaterialPreviewScreen;
 import com.goldsprite.magicdungeon.screens.tests.NeonGenTestScreen;
 import com.goldsprite.magicdungeon.screens.tests.TexturePreviewScreen;
 import java.util.Map;
-import com.goldsprite.magicdungeon.screens.tests.StarAssault;
 
-public class TestSelectionScreen extends BaseSelectionScreen {int k31;
+public class TestSelectionScreen extends BaseSelectionScreen {int k0;
 	@Override
 	protected void initScreenMapping(Map<String, Class<? extends GScreen>> map) {
 		map.put("功能验证", null);
-		map.put("bob游戏演示", StarAssault.class);
 		map.put("双网格演示", DualGridDemoScreen.class);
 		map.put("新材质预览", MaterialPreviewScreen.class);
 		map.put("纹理预览", TexturePreviewScreen.class);
