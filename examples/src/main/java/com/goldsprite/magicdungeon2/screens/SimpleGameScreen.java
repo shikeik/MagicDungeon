b8388a81d9d68a45bb490c38032a1151f5ccf5e2
shikeik <2667097598@qq.com>
2026-02-21 18:55:52 +0800

    fix: 移除未使用的Vector2导入

--- examples/src/main/java/com/goldsprite/magicdungeon2/screens/SimpleGameScreen.java
+++ examples/src/main/java/com/goldsprite/magicdungeon2/screens/SimpleGameScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
